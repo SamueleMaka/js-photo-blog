0f32044cba1898360c9b054dc5f70b9ef7e79d21
SamueleMaka <samuele.maka.imt@gmail.com>
2025-05-12 13:52:35 +0200

    feat: titolo e data da API

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
   .then(response => {
     const images = response.data;
 
-    const cardImages = document.querySelectorAll('.card-container #card-image');
-
+    const cardImages = document.querySelectorAll('.card-image');
+    const cardDates = document.querySelectorAll('.date');
+    const cardDescriptions = document.querySelectorAll('.description');
     for (let i = 0; i < cardImages.length; i++) {
       if (images[i]) {
         cardImages[i].src = images[i].url;  
+        cardDates[i].innerHTML = images[i].date;
+        cardDescriptions[i].innerHTML = images[i].title;
       }
     }
   })
